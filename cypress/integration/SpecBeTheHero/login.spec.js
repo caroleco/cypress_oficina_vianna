6e6e0491a703820454f3bc3801b8408ba503411a
clbatista <clbatista@stefanini.com>
2020-05-22 16:17:43 -0300

    testes novos

--- cypress/integration/SpecBeTheHero/login.spec.js
+++ cypress/integration/SpecBeTheHero/login.spec.js
@@ -2,18 +2,10 @@ describe('ok', () => {
     beforeEach(() => {
         cy.visit('http://localhost:3000/')
     })
-    it('Não tenho cadastro', () => {
-        cy.get('.back-link').click()
-        cy.get('input[name="ong name"]').type('testenad')
-        cy.get('#email').type('carol@t.com')
-        cy.get('input[placeholder=Whatsapp]').type('32985848548')
-        cy.get('#city').type('Juiz de Fora')
-        cy.get('#uf').type('MG')
-        cy.contains('Cadastrar').click()
-        cy.get('.success').should('contain', 'Seu ID de acesso')
-    })
 
-    it.only('Login', () => {
+    it('Login', () => {
+        //exemplo fixture
+
         cy.server()
         cy.route('POST', 'http://localhost:3333/session').as('postSession')
 
@@ -28,5 +20,35 @@ describe('ok', () => {
         cy.get('h1').contains('Faça seu logon')
 
 
+    })
+
+    it('Login com fixture', () => {
+        cy.server()
+        cy.route('POST', 'http://localhost:3333/session').as('postSession')
+
+        cy.fixture('login').then((login) => {
+            cy.get('input[name=user-id]').type(login.user_id, { log: false })
+        })
+
+
+        cy.get('button').contains('Entrar').click().wait('@postSession').then((xhr) => {
+            assert.equal(xhr.response.body.name, 'APAD')
+        }).and('have.property', 'status', 200)
+
+        cy.url().should('include', 'profile')
+        cy.get('.exit').click()
+        cy.get('h1').contains('Faça seu logon')
+    })
+
+    it.only('Não tenho cadastro', () => {
+        cy.get('.back-link').click()
+        cy.get('input[name="ong name"]').type('testenad')
+        cy.get('#email').type('carol@t.com')
+        cy.get('input[placeholder=Whatsapp]').type('32985848548')
+        cy.get('#city').type('Juiz de Fora')
+        cy.get('#uf').type('MG')
+        cy.contains('Cadastrar').click()
+        cy.get('.success').should('contain', 'Seu ID de acesso')
+        cy.get('#success-message span:first').should('not.be.empty')
     })
 })
--- cypress/integration/SpecBeTheHero/novo-caso.spec.js
+++ cypress/integration/SpecBeTheHero/novo-caso.spec.js
@@ -1,3 +1,12 @@
+var rn = require('random-number');
+var options = {
+    min: 1
+    , max: 999
+    , integer: true
+}
+
+let random_number = rn(options);
+
 describe('Novo caso', () => {
     beforeEach(() => {
         cy.visit('http://localhost:3000/')
@@ -8,16 +17,47 @@ describe('Novo caso', () => {
     it('Cadastrando com sucesso', () => {
         cy.get('#new-incident').click()
         cy.get('input[placeholder=Título]').type('ok title')
-        cy.get('textarea').type('Tetando a aplicação')
+        cy.get('textarea').type('Testando a aplicação')
         cy.get('#valor').type('120.00')
         cy.get('button[type=submit]').click()
         cy.get('li').should('contain', 'ok title')
         cy.get('li strong').contains('CASO').next().should('contain', 'ok title')
     })
+
+    it('Cadastrando e excluindo um caso', () => {
+        cy.get('#new-incident').click()
+        cy.get('input[placeholder=Título]').type(`Exclusão ${random_number}`)
+        cy.get('textarea').type('Excluir')
+        cy.get('#valor').type('120.00')
+        cy.get('button[type=submit]').click()
+        cy.get('li').contains(`Exclusão ${random_number}`).parent().within(() => {
+            cy.get('button').click()
+        })
+        cy.get('li strong').contains('CASO').next().should('not.contain', `Exclusão ${random_number}`)
+    })
+
+    it('ERRADO - Cadastrando e excluindo um caso pelo ID do ícone', () => {
+        cy.get('#new-incident').click()
+        cy.get('input[placeholder=Título]').type('Exclusão')
+        cy.get('textarea').type('Excluir')
+        cy.get('#valor').type('120.00')
+        cy.get('button[type=submit]').click()
+        cy.get('button[id^=trash]').eq(1).click()
+    })
+
     it('Cadastrando sem valor e verificando mensagem de erro', () => {
         cy.get('#new-incident').click()
         cy.get('input[placeholder=Título]').type('ok title')
         cy.get('textarea').type('Testando a aplicação')
+        cy.get('button[type=submit]').click()
+        cy.get('p.error').should('contain', 'Ops! Incidente não foi cadastrado.')
+    })
+
+    it.only('Cadastrando inserindo string no campo de Valor', () => {
+        cy.get('#new-incident').click()
+        cy.get('input[placeholder=Título]').type('ok title')
+        cy.get('textarea').type('Testando a aplicação')
+        cy.get('#valor').type('teste string')
         cy.get('button[type=submit]').click()
         cy.get('p.error').should('contain', 'Ops! Incidente não foi cadastrado.')
     })
